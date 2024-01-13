7d85295a687eccefd5f10f0c198228d2f9eb34a9
Yeong Hwan Kim <kch8317@gmail.com>
2024-01-14 01:04:42 +0900

    header js

---
+++ js/index.js
@@ -0,0 +1,119 @@
+$(document).ready(function(){
+    // $(".gnbBtn").click(function(){
+    //     $(".gnb_hidden").addClass("active")
+    // })
+    // $(".gnbBtn_close").click(function(){
+    //     $(".gnb_hidden").removeClass("active")
+    // })
+
+
+    // $(window).scroll(function(){
+    //     let sct = $(document).scrollTop()
+    //     console.log(sct)
+    // })
+ 
+    // 일반pc
+    if($(window).width() > 1439){ 
+        $(window).scroll(function () {
+             var sct = $(window).scrollTop();
+             if(sct>1100){
+                     $('header').addClass('on')
+                }
+            if(sct<1145){
+                    $('header').removeClass('on')
+                }      
+        // console.log(height);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+    })}
+
+    // 탭버전
+    if(1023< $(window).width() < 1440){ 
+        $(window).scroll(function () {
+            var sct = $(window).scrollTop();
+            if(sct>920){
+                    $('header').addClass('on')
+                }
+            if(sct<920){
+                   $('header').removeClass('on')          
+                }
+        // console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+    })}
+
+    if(767< $(window).width() < 1024){ 
+        $(window).scroll(function () {
+            var sct = $(window).scrollTop();
+            if(sct>700){
+                    $('header').addClass('on')
+                }
+            if(sct<700){
+                    $('header').removeClass('on')
+                }
+        console.log(sct)	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+    })}
+
+    if(480< $(window).width() < 768){ 
+        $(window).scroll(function () {
+            var sct = $(window).scrollTop();
+            if(sct>615){
+                    $('header').addClass('on')
+                }
+            if(sct<615){
+                    $('header').removeClass('on')
+                }
+        // console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+    })}
+
+
+
+
+
+
+
+
+
+
+
+
+    // let train = $(".banner_imgList")
+    // let trainLi = train.children()
+    // let trainLength = trainLi.length
+    
+    // let des =  $(".banner_desList")
+    // let desLi = des.children()
+
+    // let count = 0
+
+
+    
+    // let stopSlide = setInterval(function(){
+    //     count++
+    //     if(count>(trainLength - 1)){count=0}
+    //     trainLi.removeClass("on")
+    //     trainLi.eq(count).addClass("on")
+
+    //     desLi.removeClass("on")
+    //     desLi.eq(count).addClass("on")
+    // }, 4200)
+
+    // trainLi.click(function(){
+    //     clearInterval(stopSlide)
+    //     let idx = $(this)
+    //     trainLi.removeClass("on")
+    //     trainLi.eq(this).addClass("on")
+    //     desLi.removeClass("on")
+    //     desLi.eq(idx).addClass("on")
+    //     // clearInterval(stopSlide)
+    // })
+    // $(".viewstyle>span").click(function(){
+    //     $(".viewstyle>span").removeClass('on')
+    //     $(this).addClass("on")
+    // })
+
+    // $(".category>span").click(function(){
+    //     $(".category>span").removeClass('on')
+    //     $(this).addClass("on")
+    // })
+    
+
+
+
+})
