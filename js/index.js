42195d98e99f0cfe9688a96814a084bf18998d47
Yeong Hwan Kim <kch8317@gmail.com>
2024-01-17 23:45:55 +0900

    edit

--- js/index.js
+++ js/index.js
@@ -17,66 +17,139 @@ $(document).ready(function(){
     // })
  
     // 일반pc
+
+//     let ww=$(window).width()
+//     function (){ 
+//     if(1441<ww){ 
+//         $(window).scroll(function () {
+//             var sct = $(window).scrollTop();
+            
+//             if(sct>20){$('header').addClass('on')}
+//             if(sct<10){$('header').removeClass('on')}
+
+//             if(sct>320){$('.int_box').addClass('left_to_right')}
+//             if(sct>500){$('.new_family').addClass('right_to_left')}
+//             if(sct>1500){$('.w_content').addClass('u_to_d')}
+            
+//         console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+//     })}else if(1024<ww<1440){ 
+//         $(window).scroll(function () {
+//             var sct = $(window).scrollTop();
+//             if(sct>150){$('.int_box').addClass('left_to_right')}
+//             if(sct>700){$('.new_family').addClass('right_to_left')}
+//             if(sct>1300){$('.w_content').addClass('u_to_d')}
+
+//         console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+//     })}else if(779<ww<1024){
+//         $(window).scroll(function () {
+//             var sct = $(window).scrollTop();
+//             if(sct>50){$('.int_box').addClass('left_to_right')}
+//             if(sct>500){$('.new_family').addClass('right_to_left')}
+//             if(sct>1000){$('.w_content').addClass('u_to_d')}
+            
+//             console.log(sct)
+//         }
+//         )
+        
+//         $('.gnb>li>a').click(function(e){
+//             e.preventDefault()
+//         }
+//     )}else if(479 < ww < 780){ 
+//         $(window).scroll(function () {
+//             var sct = $(window).scrollTop();
+//             if(sct>450){$('.int_box').addClass('left_to_right')}
+//             if(sct>350){$('.new_family').addClass('right_to_left')}
+//             if(sct>950){$('.w_content').addClass('u_to_d')}
+//                 $('.gnb>li>a').click(function(e){
+//                     e.preventDefault()
+//                 })
+//         console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+//     }
+//     )}else if(ww < 480){ 
+//         $(window).scroll(function () {
+//             var sct = $(window).scrollTop();
+//             if(sct>120){$('.int_box').addClass('left_to_right')}
+//             if(sct>850){$('.new_family').addClass('right_to_left')}
+//             if(sct>950){$('.w_content').addClass('u_to_d')}
+//                 $('.gnb>li>a').click(function(e){
+//                     e.preventDefault()
+//                 })
+//         console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+//     }
+//     )}
+// }
+$(window).scroll(function () {
+    var sct = $(window).scrollTop();
+    
+    if(sct>20){$('header').addClass('on')}
+    if(sct<10){$('header').removeClass('on')}
+})
+
+$('.gnb>li>a').click(function(e){
+    e.preventDefault()
+})
+
+function 함수이름(){
+    var ww = $(window).width();
+    if (ww >= 1440) {
         $(window).scroll(function () {
-             var sct = $(window).scrollTop();
-             if(sct>20){
-                     $('header').addClass('on')
-                }
-            if(sct<10){
-                    $('header').removeClass('on')
-                }      
-        // console.log(height);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+            var sct = $(window).scrollTop();
+
+            if(sct>320){$('.int_box').addClass('left_to_right')}
+            if(sct>600){$('.new_family').addClass('right_to_left')}
+            if(sct>1400){$('.w_content').addClass('u_to_d')}
+            
+        console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+    })
+    }else if(1024 <= ww <1440) {
+        $(window).scroll(function () {
+            var sct = $(window).scrollTop();
+
+            if(sct>150){$('.int_box').addClass('left_to_right')}
+            if(sct>600){$('.new_family').addClass('right_to_left')}
+            if(sct>1100){$('.w_content').addClass('u_to_d')}
+
+        console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+    })
+    }else if(768 <= ww <1024) {
+        $(window).scroll(function () {
+            var sct = $(window).scrollTop();
+
+            if(sct>50){$('.int_box').addClass('left_to_right')}
+            if(sct>550){$('.new_family').addClass('right_to_left')}
+            if(sct>1000){$('.w_content').addClass('u_to_d')}
+            
+        console.log(sct);
+        })
+    }else if(480 <= ww <768) {
+        $(window).scroll(function () {
+            var sct = $(window).scrollTop();
+
+            if(sct>450){$('.int_box').addClass('left_to_right')}
+            if(sct>350){$('.new_family').addClass('right_to_left')}
+            if(sct>950){$('.w_content').addClass('u_to_d')}
+               
+        console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
     })
 
-    // // 탭버전
-    // if(1023< $(window).width() < 1440){ 
-    //     $(window).scroll(function () {
-    //         var sct = $(window).scrollTop();
-    //         if(sct>920){
-    //                 $('header').addClass('on')
-    //             }
-    //         if(sct<920){
-    //                $('header').removeClass('on')          
-    //             }
-    //     // console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
-    // })}
+ 
+    }else if(480 > ww ) {
+        $(window).scroll(function () {
+            var sct = $(window).scrollTop();
 
-    if(767<$(window).width()<1024){
-        $('.gnb>li>a').click(function(e){
-            e.preventDefault()
-        }
-        )}
-        // $(window).scroll(function () {
-        //     var sct = $(window).scrollTop();
-        //     if(sct>700){
-        //             $('header').addClass('on')
-        //         }
-        //     if(sct<700){
-        //             $('header').removeClass('on')
-        //         }
-        // console.log(sct)	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
-    // )}
+            if(sct>120){$('.int_box').addClass('left_to_right')}
+            if(sct>850){$('.new_family').addClass('right_to_left')}
+            if(sct>950){$('.w_content').addClass('u_to_d')}
+              
+        console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
+    
+            
+    })
+ 
+    }
 
-    // if(480< $(window).width() < 768){ 
-    //     $(window).scroll(function () {
-    //         var sct = $(window).scrollTop();
-    //         if(sct>590){
-    //                 $('header').addClass('on')
-    //             }
-    //         if(sct<590){
-    //                 $('header').removeClass('on')
-    //             }
-                // $('.gnb>li>a').click(function(e){
-                //     e.preventDefault()
-                // })
-    //     // console.log(sct);	// 스크롤할때마다 해당 스크롤 위치 좌표값(높이)
-    // }
-    // )}
-
-
-
-
-
+}
+함수이름()
 
 
 
